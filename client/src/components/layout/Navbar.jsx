70f7269143533a26b1bbe6346962f1fb434dc9d0
Sumit Dhimal <sumit.dhimal.333@gmail.com>
2026-08-02 14:22:12 +0545

    feature: sidebar navigation

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,36 +1,45 @@
+import { useState } from "react";
 import logo from "../../assets/logo.png";
 import "../components.css";
+import SideNav from "./SideNav";
 
 import { LuAlignJustify } from "react-icons/lu";
 import { LuSearch } from "react-icons/lu";
 import { LuHeart } from "react-icons/lu";
 import { LuShoppingCart } from "react-icons/lu";
 
-
-
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav
-      className="w-full fixed top-0 left-0 z-50"
-    > 
-      <div className="mx-auto h-20 flex justify-between max-w-340 items-center px-6 py-12">
-        <div>
-          <LuAlignJustify className="nav-btns" />
+    <>
+      <nav
+        className="w-full fixed top-0 left-0 z-40"
+      > 
+        <div className="mx-auto h-20 flex justify-between max-w-340 items-center px-6 py-12">
+          <div
+            onClick={() => setIsOpen(true)}
+          >
+            <LuAlignJustify className="nav-btns" />
+          </div>
+
+          <img 
+            src={logo} 
+            alt="logo"  
+            className="w-fit h-20 cursor-pointer"
+          />
+
+          <div className="flex gap-4 items-center">
+            <LuSearch className="nav-btns" />
+            <LuHeart className="nav-btns" />
+            <LuShoppingCart className="nav-btns" />
+          </div>
         </div>
 
-        <img 
-          src={logo} 
-          alt="logo"  
-          className="w-fit h-20 cursor-pointer"
-        />
+      </nav>
 
-        <div className="flex gap-6 items-center">
-          <LuSearch className="nav-btns" />
-          <LuHeart className="nav-btns" />
-          <LuShoppingCart className="nav-btns" />
-        </div>
-      </div>
-    </nav>
+      <SideNav isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   )
 };
 
---
+++ client/src/components/layout/SideNav.jsx
@@ -0,0 +1,124 @@
+import { LuUser, LuX } from "react-icons/lu";
+import '../components.css';
+import { Link } from "react-router-dom";
+
+import {FaDiscord, FaInstagram, FaYoutube, FaGithub} from "react-icons/fa";
+import { LuCircleUserRound } from "react-icons/lu";
+
+const categories = [
+  {
+    title: "Cosplay"
+  }, 
+  {
+    title: "Clothing"
+  }, 
+  {
+    title: "Jewellery"
+  }, 
+  {
+    title: "Accessories"
+  }, 
+  {
+    title: "Collectibles & Decor"
+  },
+  {
+    title: "Bizzare Anime Finds"
+  } 
+];
+const supports = ["Coupons & Offers", "Photos & Reviwes", "Track Order", "Need Help?", "About Us"];
+
+const SideNav = ({isOpen, onClose}) => {
+  return (
+    <>
+
+      {/* side navigation */}
+      <aside
+        className={`
+          fixed top-0 left-0
+          min-w-lg h-screen shadow-xl bg-violet-500 z-50
+          transition-transform duration-300
+          ${isOpen? "translate-x-0": "-translate-x-full"}
+        `}
+          
+      > 
+
+        {/* close btn */}
+        <div className="flex justify-end p-4">
+          <button
+            onClick={onClose}
+            className="text-gray-50 border-2 border-gray-50 hover:border-pink-500 transition-colors ease-in-out rounded-md p-1"
+          >
+            <LuX className="text-2xl cursor-pointer" />
+          </button>
+        </div>
+        
+        {/* social btns */}
+        <div className="flex gap-3 mx-8 mt-8 border-b-2 border-gray-50 pb-4">
+          <FaDiscord className="sidebar-btns" />
+          <FaInstagram  className="sidebar-btns" />
+          <FaYoutube className="sidebar-btns" />
+          <FaGithub className="sidebar-btns" />
+        </div>
+        
+        {/* Navigation btns */}
+        <div className="flex gap-1 mx-8 mt-4">
+          <button className="sidebar-black-btns">New Arrivals</button>
+          <button className="sidebar-black-btns">Best Sellers</button>
+          <button className="sidebar-black-btns">Clearance Sale</button>
+        </div>
+        
+        {/* Categories links */}
+        <div className="py-8 mx-8 border-b-2 border-gray-50">
+          <ul className="flex flex-col gap-2">
+          {
+            categories.map((cateory, idx) => (
+              <li 
+                key={idx}
+                className="sidebar-links"
+              >
+                <Link to="123">
+                  {cateory.title}
+                </Link>
+              </li>
+            )
+            )
+          }
+          </ul>
+        </div>
+
+        {/* login and others */}
+        <div className="py-6 mx-8">
+
+          {/* login */}
+          <div className="
+            pb-8 w-fit flex items-center gap-2 
+            text-sm text-gray-50 hover:text-pink-600 cursor-pointer
+          ">
+            <LuCircleUserRound size={24} />
+
+            <p className="uppercase font-extralight">
+              Login/Register
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            {
+              supports.map((item, idx) => (
+                <Link
+                  key={idx}
+                  to="123"
+                  className="sidebar-links text-sm"
+                >
+                  { item }
+                </Link>
+              ))
+            }
+          </div>
+        </div>
+
+      </aside>
+    </>
+  );
+};
+
+export default SideNav;
